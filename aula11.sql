065ca09c344805f9afe776e1ec95c665d4f77356
Willian-Garcia <williangarcia710@hotmail.com>
2023-10-06 22:30:13 -0300

    Atualização Aula 10

--- aula11.sql
+++ aula11.sql
@@ -1,5 +1,5 @@
-create database bd_aula10;
-\c bd_aula10
+create database bd_aula11;
+\c bd_aula11
 
 create table tbl_cidades(
 cod_cidade serial primary key,
@@ -184,59 +184,21 @@ INSERT INTO tbl_empregados (nome, data_nascimento, endereco, sexo, salario, cod_
 -- exercicios
 
 
---1 crie uma view chamada v_depcidade que liste o nome de cada departamento com o nome da cidade onde este departamento está localizado.
---Após a criacao executar Select * from v_depcidade; retorna 36 linhas
-CREATE OR REPLACE VIEW v_depcidade AS
-SELECT d.nome AS departamento, c.nome AS cidade
-FROM tbl_departamentos d
-INNER JOIN tbl_cidades c ON d.cod_cidade = c.cod_cidade;
+--1 crie uma store procedure chamada proc_upd_nome_depart para atualizar o nome de um departamento. 
+--recebe como parametro um codigo inteiro e um novonome em texto atualizando o nome do departamento com esse respectivo codigo
 
-SELECT * FROM v_depcidade;
 
---2 crie uma view denominada v_depcidadehouston, a partir de v_depcidade que mostre somente os departamentos localizados em Houston
--- execute select * from v_depcidadehouston
-CREATE OR REPLACE VIEW v_depcidadehouston AS
-SELECT departamento, cidade
-FROM v_depcidade
-WHERE cidade = 'Houston';
+--2 crie uma store procedure chamada proc_copiatbl que cria uma copia da tabela tbl_cidades toda vez que for executada;
 
-SELECT * FROM v_depcidadehouston;
 
---3 Crie uma visão denominada de v_opsalario, a qual lista a soma e média de todos os salários dos empregados.
--- execute select * from v_opsalario
-CREATE OR REPLACE VIEW v_opsalario AS
-SELECT SUM(salario) AS soma_salarios, AVG(salario) AS media_salarios
-FROM tbl_empregados;
+--3 crie uma store procedure chamada proc_novoprojeto que adiciona um novo projeto na tabela tbl_projetos
+--recebe como parametros o nome do projeto e o codigo do departamento
 
-SELECT * FROM v_opsalario;
 
---4 Criar a view vw_empregados_salarial que exibe o nome e o salário de todos os empregados com salário acima de 5500.
--- execute select * from v_empregados_salarial
-CREATE OR REPLACE VIEW v_empregados_salarial AS
-SELECT nome, salario
-FROM tbl_empregados
-WHERE salario > 5500.00;
+--4 crie uma store procedure chamada proc_delprojeto que deleta um projeto da tbl_projetos
+-- recebe como parametro o codigo do projeto
 
-SELECT * FROM v_empregados_salarial;
 
---5 criar uma view v_departemp para listar os departamentos e a quantidade de empregados em cada um
--- listar os departamentos e as quantidades por orderm decrescente de quantidade.
-CREATE OR REPLACE VIEW v_departemp AS
-SELECT d.nome AS departamento, COUNT(e.cod_empregado) AS quantidade_empregados
-FROM tbl_departamentos d
-INNER JOIN tbl_empregados e ON d.cod_departamento = e.cod_departamento
-GROUP BY d.nome
-ORDER BY quantidade_empregados DESC;
-
-SELECT * FROM v_departemp;
-
---6 criar uma view v_departrouble para listar os nomes dos departamentos que possuem mais projetos do que empregados.
-CREATE OR REPLACE VIEW v_departrouble AS
-SELECT d.nome AS departamento
-FROM tbl_departamentos d
-LEFT JOIN tbl_projetos p ON d.cod_departamento = p.cod_departamento
-LEFT JOIN tbl_empregados e ON d.cod_departamento = e.cod_departamento
-GROUP BY d.nome
-HAVING COUNT(p.*) > COUNT(e.*);
-
-SELECT * FROM v_departrouble;
+--5 crie uma store procedure chamada proc_projeto_arquivado que recebe o codigo de um projeto.
+--a procedure devera criar uma tabela chamada tbl_projetos_arquivados, caso ela nao exista. tabela deve ter 2 colunas: codigo_projeto e nome.
+--a procedure deve salvar o projeto do codigo recebido na tbl_projetos_arquivados e deleta-la da tabela tbl_projetos.
